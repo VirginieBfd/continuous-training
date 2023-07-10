7959f0074aa7c6bff9cbffc827c5dfd07f9754c6
Virginie <virginie@192.168.1.119>
2023-07-10 10:20:38 +0100

    feat(.github/workflows/docker.yml): add step to display output from python file

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("Hello world! Today is a beautiful day, isn't it?")
+def main():
+  output = "Hello world! Today is a beautiful day, isn't it?"
+  return output
+
+if __name__ == "__main__":
+  print(main())
