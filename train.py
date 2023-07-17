dd937293da3d90bcae8e786f0bb7ab4ce2f8da41
Virginie <virginie@192.168.1.125>
2023-07-17 10:40:19 +0100

    feat(train.py): add MNISTDataModule to split data processing from training

--- train.py
+++ train.py
@@ -10,11 +10,52 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
-BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+
+import lightning.pytorch as pl
+from torch.utils.data import random_split, DataLoader
+
+# Note - you must have torchvision installed for this example
+from torchvision.datasets import MNIST
+from torchvision import transforms
 
 
-class MNISTModel(L.LightningModule):
+class MNISTDataModule(L.LightningDataModule):
+    def __init__(self, data_dir: str = "./"):
+        super().__init__()
+        self.data_dir = os.environ.get("PATH_DATASETS", ".")
+        self.batch_size = 256 if torch.cuda.is_available() else 64
+        self.transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+    def prepare_data(self):
+        # download
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit":
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test":
+            self.mnist_test = MNIST(
+                self.data_dir, train=False, transform=self.transform
+            )
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+
+
+class Model(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.l1 = torch.nn.Linear(28 * 28, 10)
@@ -46,13 +87,8 @@ class MNISTModel(L.LightningModule):
 
 
 # Init our model
-model = MNISTModel()
-
-# Init DataLoader from MNIST Dataset
-train_ds = MNIST(
-    PATH_DATASETS, train=True, download=True, transform=transforms.ToTensor()
-)
-train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE)
+model = Model()
+dm = MNISTDataModule()
 
 # Initialize a trainer
 wandb_logger = WandbLogger(project="mlops-wandb", log_model=True)
@@ -68,4 +104,7 @@ trainer = L.Trainer(
 
 
 # Train the model ⚡
-trainer.fit(model, train_loader)
+trainer.fit(model, dm)
+
+# Evaluate the model ⚡
+trainer.test(model, dm)
